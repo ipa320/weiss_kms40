7fabb3ece24ba8a1a50cf975b7c8829cc1a81f42
lth-sg <lth-sg@Ara>
2013-08-12 16:17:02 +0200

    Test Listener that listens to the TF and get the Rotation matrix using the getRotation() Function and calculate the force transformation between two frames.

--- ipa325_kms40/src/Transform_Publisher.cpp
+++ ipa325_kms40/src/Transform_Publisher.cpp
@@ -1,23 +1,65 @@
 #include <iostream>
 #include <string>
-
-
-#include "ros/ros.h"
-#include <tf/transform_broadcaster.h>
 #include <geometry_msgs/WrenchStamped.h>
 
+#include "ros/ros.h"
+#include <tf/transform_listener.h>
+#include<tf/tf.h>
+#include <geometry_msgs/PointStamped.h>
 
-void transformCallback(const tf::tfMessage::ConstPtr& data)
+
+
+void Callback( const geometry_msgs::WrenchStamped::ConstPtr& data)
 {
 
+   F[0]= data -> wrench.force.x;
+   F[1]= data -> wrench.force.y;
+   F[2]= data -> wrench.force.z;
+
+   tf::TransformListener listener;
+
+   ros::Rate rate(10.0);
+
+   while(node.ok())
+   {
+       tf::StampedTransform transform;
+
+       try{
+
+           listener.lookupTransform("/kms400","/new_frame",ros::Time(0),transform);
+       }
+       catch (tf::TransformException ex)
+       {
+
+           ROS_ERROR("%s",ex.what());
+
+       }
+
+       rot_matrix=transform.getBasis();
+
+   }
 
 
-    data->transforms.data();
+   for(int j=0;j<3;j++)
+   {
+          Fn[j]=0;
 
+          for(int k=0;k<3;k++)
+           {
+
+               Fn[j]=Fn[j]+rot_matrix[j][k] * F[k];
+
+           }
+
+   }
+
+   cout<<"New Force Matrix"<<std::endl;
+   for(int j=0;j<3;j++)
+   {
+       cout<<Fn[j]<<"/t";
+   }
 
 }
-
-
 
 int main(int argc, char **argv)
 {
@@ -25,8 +67,12 @@ int main(int argc, char **argv)
     ros::init( argc , argv , "Transform_Publisher");
     ros::NodeHandle n;
 
-    ros::Subscriber transform = n.subscribe("/tf",1000,transformCallback);
+
+
+
+    ros::Subscriber sub =n.subscribe("/kms400",1000,Callback);
     ros::spin();
+
     return 0;
 
 }
