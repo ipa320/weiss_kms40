588a4b6fc6049fb48862c99c21117e58c690f194
lth <lorenz.halt@ipa.fraunhofer.de>
2020-02-10 10:51:58 +0100

    disable install (emulator) for now

--- weiss_kms40_emulator/setup.py
+++ weiss_kms40_emulator/setup.py
@@ -6,8 +6,8 @@ from catkin_pkg.python_setup import generate_distutils_setup
 
 d = generate_distutils_setup(
    ##  don't do this unless you want a globally visible script
-   # scripts=['bin/myscript'], 
-   packages=['PYTHON_PACKAGE_NAME'],
+   # scripts=['bin/myscript'],
+   packages=['weiss_kms40_emulator'],
    package_dir={'': 'src'}
 )
 
